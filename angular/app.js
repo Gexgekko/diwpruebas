68a24d148096e18a1249e88c598f16a463bddd7b
gexgekko <scgexx@gmail.com>
2015-04-13 20:59:19 +0200

    Mas pruebas de ngshow

--- angular/app.js
+++ angular/app.js
@@ -1,8 +1,7 @@
 var diwApp = angular.module('diwApp',['ui.router']);
 
 diwApp.controller('inicioCtrl',function($scope){
-	var vm = $scope;
-	vm.muerte = function(numero){
+	$scope.muerte = function(numero){
 		vm.nuerteNum = numero;
 	}
 })
