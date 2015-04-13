1d1cbec77bd72122aeebd58d55bb3f52a2618365
gexgekko <scgexx@gmail.com>
2015-04-13 20:55:23 +0200

    Probando el controller de las muertes, aun solo usando un ng-show

--- angular/app.js
+++ angular/app.js
@@ -1,1 +1,8 @@
 var diwApp = angular.module('diwApp',['ui.router']);
+
+diwApp.controller('inicioCtrl',function($scope){
+	var vm = $scope;
+	vm.muerte = function(numero){
+		vm.nuerteNum = numero;
+	}
+})
