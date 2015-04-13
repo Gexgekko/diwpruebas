b5e72127a6a088698a350def77fcf49b31ad1722
gexgekko <scgexx@gmail.com>
2015-04-13 22:43:48 +0200

    Muertes listas Inicio

--- angular/app.js
+++ angular/app.js
@@ -1,7 +1,15 @@
 var diwApp = angular.module('diwApp',['ui.router']);
 
-diwApp.controller('inicioCtrl',function($scope){
-	$scope.muerte = function(numero){
+diwApp.controller('baseCtrl',function($scope, $state, $stateParams){
+	$scope.vivo = 1;
+	$scope.tituloAventura = 'Inicio';
+	$scope.muerte = function(numero, texto){
 		$scope.muerteNum = numero;
+		$scope.tituloAventura = texto;
+		$scope.vivo = 0;
+	}
+	$scope.resucitar = function(){
+		$scope.muerteNum = 0;
+		$scope.vivo = 1;
 	}
 })
