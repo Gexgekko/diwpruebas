82888945127e49513db87e2eb450f559ebaef23a
gexgekko <scgexx@gmail.com>
2015-04-13 23:37:26 +0200

    Prueba con la pagina 2 y cambios en tituloAventura

--- angular/baseCtrl.js
+++ angular/baseCtrl.js
@@ -1,7 +1,12 @@
 diwApp.controller('baseCtrl',function($scope, $state){
 	$state.go('inicio');
 	$scope.vivo = 1;
-	$scope.tituloAventura = 'Inicio';
+	if($state.current == 'inicio'){
+		$scope.tituloAventura = 'Inicio';	
+	}
+	if($state.current == 'segundo'){
+		$scope.tituloAventura = 'Cuidado!';	
+	}
 	$scope.muerte = function(numero, texto){
 		$scope.muerteNum = numero;
 		$scope.tituloAventura = texto;
