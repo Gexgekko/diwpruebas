e5b046a6ca845ea418bc52bb4970d306c214d9c9
gexgekko <scgexx@gmail.com>
2015-04-13 21:01:34 +0200

    Cambia app.js

--- angular/app.js
+++ angular/app.js
@@ -2,6 +2,6 @@ var diwApp = angular.module('diwApp',['ui.router']);
 
 diwApp.controller('inicioCtrl',function($scope){
 	$scope.muerte = function(numero){
-		vm.nuerteNum = numero;
+		$scope.nuerteNum = numero;
 	}
 })
