293ead2573adb78ea9c3600a34f1ba85fab8eaff
Gexgekko <scgexx@gmail.com>
2015-04-26 17:20:57 +0200

    Primera prueba con cookies

--- pruebasLimpias/cookies.js
+++ pruebasLimpias/cookies.js
@@ -1,6 +1,12 @@
-var diwCookieApp = angular.module('diwCookieApp',[]);
-diwCookieApp.controller('CookieCtrl',function($scope){
+var diwCookieApp = angular.module('diwCookieApp',['ngCookies']);
+diwCookieApp.controller('CookieCtrl',function($scope,$cookieStore){
 	$scope.verTexto = function (){
 		$scope.textoVer = $scope.textoIntroducido;
+	}
+	$scope.guardarCookie = function (){
+		$cookieStore.put('DIWCookie1', $scope.cookieCargada);
+	}
+	$scope.cargarCookie = function (){
+		$scope.cookieLeida = $cookieStore.get('DIWCookie1');
 	}
 });
